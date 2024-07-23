3c552e6700ea1e21fc86556eee94c87bdfb0898d
Rotherick <rotherickcalderon.git@gmail.com>
2024-07-23 17:32:47 -0400

    Inicio Rama 1

---
+++ Inicio.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Mi Página de Inicio en PHP</title>
+    <style>
+        body {
+            background-color: White;
+            color: Black;
+            font-family: Arial, sans-serif;
+        }
+        header {
+            color: #00f; /* Color azul eléctrico */
+            text-align: center;
+            padding: 20px;
+            font-size: 24px;
+        }
+        .content {
+            text-align: center;
+            margin-top: 50px;
+            font-size: 18px;
+        }
+    </style>
+</head>
+<body>
+    <header>UNIVERSIDAD PRIVADA DOMINGO SAVIO</header>
+    <div class="content">HOLA SOY ROTHERICK CALDERON</div>
+    <div class="content">RAMA 1</div>
+
+</body>
+</html>
